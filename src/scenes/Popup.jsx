dcf369628a1e357cc208fb4197f0e2b4a04f260c
babinicz <babinicz@onet.pl>
2025-02-03 18:52:04 +0100

    zm

--- src/scenes/Popup.jsx
+++ src/scenes/Popup.jsx
@@ -3,7 +3,7 @@ import { FaTimes } from "react-icons/fa"; // Import
 const Popup = ({ closePopup }) => {
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-lg w-11/12 sm:w-4/5 md:w-1/2 lg:w-2/5 xl:w-1/3 relative min-h-[500px] h-auto">
+      <div className="bg-white p-6 rounded-lg w-11/12 sm:w-4/5 md:w-1/2 lg:w-2/5 xl:w-1/3 relative h-auto">
         <button
           onClick={closePopup}
           className="absolute top-4 right-4 text-2xl text-gray-700 hover:text-red-500"
