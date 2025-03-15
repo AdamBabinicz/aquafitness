382f1fc81c3873b6db028fa3e4c901767b8ec792
babinicz <babinicz@onet.pl>
2025-03-15 12:47:04 +0100

    zmiany

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
           <h3 className="font-playfair font-semibold text-2xl text-yellow">
-            Aqua Fitness 55+
+            Aqua Fitness Senior
           </h3>
           <h4 className="font-playfair text-md text-yellow">
             Radom 2025 - {new Date().getFullYear()}.
