0ec5330311bc99a3d74f4897cdde99dde62806e0
babinicz <babinicz@onet.pl>
2025-03-27 19:27:19 +0100

    zamiana fotek

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -31,7 +31,7 @@ const Landing = ({ setSelectedPage }) => {
             <img
               alt="trenerka"
               className="relative hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]absolute top-[-30px] left-0"
-              src="assets/mariola2.avif"
+              src="assets/11.avif"
               loading="lazy"
             />
           </div>
@@ -39,7 +39,7 @@ const Landing = ({ setSelectedPage }) => {
           <img
             alt="profile"
             className="z-10 w-full max-w-[400px] md:max-w-[600px]"
-            src="assets/11.avif"
+            src="assets/mariola2.avif"
           />
         )}
       </div>
