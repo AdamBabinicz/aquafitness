cfd7e639024744b03139931433bb5285ec6c41b7
babinicz <babinicz@onet.pl>
2025-07-19 12:41:30 +0200

    mirka

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
         person6: "url('./assets/1.avif')",
         person7: "url('./assets/elka.avif')",
         person8: "url('./assets/3.avif')",
-        person9: "url('./assets/11.avif')",
+        person9: "url('./assets/mir.avif')",
         person10: "url('./assets/1-2.avif')",
         person11: "url('./assets/1-4.avif')",
         person12: "url('./assets/14.avif')",
